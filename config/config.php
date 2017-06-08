25a3203dc5664f9ed5682e0b9160599b9a19358b
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-08 13:20:43 +0100

    Abstract form handler from register.php to register_handler.php.

--- config/config.php
+++ config/config.php
@@ -8,7 +8,8 @@ $time_zone = date_default_timezone_set("Europe/London");
 $con = mysqli_connect("localhost","root","","social"); /*Connection Variable*/
 
 if(mysqli_connect_errno()){
-    echo "Failed to connect: " . mysqli_connect_errno();
+   echo "Connection failed: ". mysqli_connect_errno();
+   exit();
 }
 
 ?>
