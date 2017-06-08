cea1fc14f4da178f0234e6ca9a435755a645967f
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-08 17:01:33 +0100

    Add prepared statements for logging and scaffolding for error if unsucessful.

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -10,15 +10,17 @@
         $options = [
             'cost' => 12,
         ];
-        $password = password_hash($_POST['log_password'],PASSWORD_DEFAULT,$options);
 
-
-        $match_login_credentials_query = "SELECT COUNT(*) FROM users WHERE email = ? AND password = ?";
+        /**
+         * Defensive: Check that DB integrity has not been compromised.
+         * Consider : Turning account off ???????????????????!!!!!!!!!!
+         */
+        $count_valid_login_credentials_query = "SELECT COUNT(*) FROM users WHERE email = ?";
         /*Create prepared statement*/
-        if($stmt = mysqli_prepare($con,$match_login_credentials_query)){
+        if($stmt = mysqli_prepare($con,$count_valid_login_credentials_query)){
             
             /*Bind parameters for markers, type 's'/string */
-            mysqli_stmt_bind_param($stmt, "ss",$email,$password);
+            mysqli_stmt_bind_param($stmt, "s",$email);
             /*Execute query*/
             mysqli_stmt_execute($stmt);
             /*Bind Result variables*/
@@ -30,9 +32,34 @@
         }
 
         if($result === 1){
-            echo "Found it";
+            /*Create prepared statement*/
+            $fetch_hash_query = "SELECT password FROM users WHERE email = ?";
+            if($stmt = mysqli_prepare($con,$fetch_hash_query)){
+                /*Fetch hash from DB for relevant credentials*/
+                /*Bind parameters for markers, type 's'/string */
+                mysqli_stmt_bind_param($stmt, "s",$email);
+                /*Execute query*/
+                mysqli_stmt_execute($stmt);
+                /*Bind Result variables*/
+                mysqli_stmt_bind_result($stmt, $result2);
+                /*Fetch values i.e. to bound result variable*/
+                mysqli_stmt_fetch($stmt);
+                /* Close stmt*/
+                mysqli_stmt_close($stmt);
+                echo "exiting final sql, result: ". $result2 . $email;
+        }
         }else{
-            echo "Did not find it";
+            /* 0 == Doesnt exist. 2 == Something bad has happened. Figure out a way to deal */
+            echo "Either user doesnt exist or something bad has happened !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+            exit();
+        }
+
+        if(password_verify($_POST['log_password'],$result2)){
+            echo "correct credentials, access granted";
+        }else{
+            echo "incorrect password, access denied";
+            echo "you entered" . $_POST['log_password'];
+            echo "corresponding hash:" . $result2;
         }
 
  
