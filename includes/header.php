b3ac5579f4bb70237ed90ffa09d957cd1215d908
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-10 18:16:07 +0100

    Add twitter bootstrap and jquery to header

--- includes/header.php
+++ includes/header.php
@@ -17,8 +17,27 @@ if(isset($_SESSION["username"])){
 <head>
    <meta charset="UTF-8">
    <title>TimsiFeed:Wall </title>
-   <script src="./assets/js/bootstrap.js"></script>
-   <link rel=stylesheet type="text/css" href="./assets/css/bootstrap.css"></script>
+   <!--JS-->
+   <script src="./assets/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+   <!--J Qeury-->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <!--CSS-->
+   <link rel=stylesheet type="text/css" href="./assets/bootstrap-3.3.7-dist/css/bootstrap.css"></script>
 </head>
 
 <body>
+
+
+<!-- Single button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Action <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
