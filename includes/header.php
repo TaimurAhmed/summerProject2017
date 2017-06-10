bfe4add916323a232023ededc28e3ae6bbadfaa6
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-10 19:33:38 +0100

    Add font awesome cdn and nav bar styling

--- includes/header.php
+++ includes/header.php
@@ -22,22 +22,28 @@ if(isset($_SESSION["username"])){
    <!--J Qeury-->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <!--CSS-->
-   <link rel=stylesheet type="text/css" href="./assets/bootstrap-3.3.7-dist/css/bootstrap.css"></script>
+   <link rel=stylesheet type="text/css" href="./assets/bootstrap-3.3.7-dist/css/bootstrap.css"></link>
+   <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+   <!--CSS: Font Awesome CDN-->
+   <script src="https://use.fontawesome.com/342380e526.js"></script>
 </head>
 
 <body>
 
+    <div class="top_bar">
+        
+        <div class="logo">
+            <a href="index.php"> TimsiFeed</a>
+        </div>
+        
+        <nav>
+            <a href="#"><i class="fa fa-home"></i></a>
+            <a href="#"><i class="fa fa-envelope"></i></a>
+            <a href="#"><i class="fa fa-bell-o"></i></a>
+        </nav>
+    
+    </div>
 
-<!-- Single button -->
-<div class="btn-group">
-  <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Action <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu">
-    <li><a href="#">Action</a></li>
-    <li><a href="#">Another action</a></li>
-    <li><a href="#">Something else here</a></li>
-    <li role="separator" class="divider"></li>
-    <li><a href="#">Separated link</a></li>
-  </ul>
-</div>
+
+
+
