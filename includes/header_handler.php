61937e38861b540506d79b1cb607e941db58d7bf
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-12 15:48:57 +0100

    Add person meta data to profile picture block

--- includes/header_handler.php
+++ includes/header_handler.php
@@ -1,7 +1,7 @@
 <?php
         $meta_person = array();
 
-        $get_first_name_query = "SELECT first_name,profile_pic,num_posts,num_likes FROM users WHERE username = ?";
+        $get_first_name_query = "SELECT first_name,last_name,profile_pic,num_posts,num_likes FROM users WHERE username = ?";
         /*Create prepared statement*/
         if($stmt = mysqli_prepare($con,$get_first_name_query)){
             
@@ -10,7 +10,7 @@
             /*Execute query*/
             mysqli_stmt_execute($stmt);
             /*Bind Result variables*/
-            mysqli_stmt_bind_result($stmt,$meta_person["first_name"],$meta_person["profile_pic"],$meta_person["num_posts"],$meta_person["num_likes"]);
+            mysqli_stmt_bind_result($stmt,$meta_person["first_name"],$meta_person["last_name"],$meta_person["profile_pic"],$meta_person["num_posts"],$meta_person["num_likes"]);
             /*Fetch values i.e. to bound result variable*/
             mysqli_stmt_fetch($stmt);
             /* Close stmt*/
