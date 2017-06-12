efa6439f851211f3161a58664dc768d0707fe772
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-12 16:07:57 +0100

    Add styling and finishing touches to user details column

--- index.php
+++ index.php
@@ -11,14 +11,17 @@ require './includes/header.php';
             <?php if(isset($meta_person["profile_pic"])){echo $meta_person["profile_pic"];}?>" 
            alt="">
         </a>
-        <a href="#">
-            <?php if(isset($meta_person["first_name"])){echo $meta_person["first_name"];}            
-                  if(isset($meta_person["last_name"])){echo " " . $meta_person["last_name"];}
-            ?>
-            
-        </a>
-        <?php if(isset($meta_person["num_posts"])){echo "Posts:". $meta_person["num_posts"]."<br>";}?>
-        <?php if(isset($meta_person["num_likes"])){echo "Likes:". $meta_person["num_likes"];}?>  
+
+        <div class="user_details_left_right">
+            <a href="#">
+                <?php if(isset($meta_person["first_name"])){echo $meta_person["first_name"];}            
+                      if(isset($meta_person["last_name"])){echo " " . $meta_person["last_name"];}
+                ?>
+                
+            </a>
+            <?php if(isset($meta_person["num_posts"])){echo "Posts:". $meta_person["num_posts"]."<br>";}?>
+            <?php if(isset($meta_person["num_likes"])){echo "Likes:". $meta_person["num_likes"];}?>  
+        </div>
     </div>
 
     </div>
