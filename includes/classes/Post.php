2e70b61df196c09e28c5e1ba77e8b68aeff120c1
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-13 12:48:37 +0100

    Refactor: Refactored to prepared statements.

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -9,40 +9,47 @@ class Post{
     }
 
     public function  submitPost($body,$user_to) {
-        $body = strip_tags($body); //removes html tags
+        $body = strip_tags($body); /*Remove HTML tags*/
         $body = mysqli_real_escape_string($this->con,$body);
-        $check_empty = preg_replace('/\s+/', '', $body);//delete all spaces
+        $check_empty = preg_replace('/\s+/', '', $body);/*Delete all spaces*/
 
+        /*If string is not empty insert post*/
         if($check_empty != ""){
-
-            //Current date and time
+            /*Current date and time*/
             $date_added = date("Y-m-d H:i:s");
-            //Get userName
+            /*Get userName*/
             $added_by = $this->user_obj->getusername();
-
-            //If user is not on own profile, user_to is 'none'
+            /*If user is not on own profile, user_to is set to 'none'*/
             if($user_to === $added_by){
                 $user_to = "none";
             }
+                /*Insert post into DB*/
+                $create_post_query = "INSERT INTO posts VALUES('',?,?,?,?,'no','no','0')";
+                if($stmt = mysqli_prepare($this->con,$create_post_query)){
+                    /*Bind parameters for markers, type 's'/string */
+                    mysqli_stmt_bind_param($stmt, "ssss",$body,$added_by,$user_to,$date_added);
+                    /*Execute query*/
+                    mysqli_stmt_execute($stmt);
+                    /*The auto-generated surrogate key*/
+                    $returned_id = $this->con->insert_id;
+                    /*Close prepared stmt*/
+                    mysqli_stmt_close($stmt);
 
-            //Insert post into DB
-            $query = mysqli_query($this->con,"INSERT INTO posts VALUES('','$body','$added_by','$user_to','$date_added','no','no','0')");
-            $returned_id = mysqli_insert_id($this->con);
 
-            //Insert notification
-            
-            //Update Post Count for User
-            $num_posts = $this->user_obj->getNumPosts();
-            $num_posts++;
-            $update_query = mysqli_query($this->con, "UPDATE users SET num_posts = '$num_posts' WHERE username = $added_by");
+                    /*Update Post Count for User*/
+                    $num_posts = $this->user_obj->getNumPosts();
+                    $num_posts++;
+                    $update_user_numPosts_query = "UPDATE users SET num_posts = ? WHERE username = ? ";
+                    if($stmt = mysqli_prepare($this->con,$update_user_numPosts_query)){
+                        /*Bind parameters for markers, type 's'/string */
+                        mysqli_stmt_bind_param($stmt, "ss",$num_posts,$added_by);
+                        /*Execute query*/
+                        mysqli_stmt_execute($stmt);
+                        /*Close prepared stmt*/
+                        mysqli_stmt_close($stmt);
+                    }
+                }
         }
     }
-
-
-
-
-
 }
-
-
 ?>
