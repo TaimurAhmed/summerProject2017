d79130fb18a707cec22a950e17587431a26e45a1
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-17 22:34:19 +0100

    Add functioning likes and unlikes with echo tests inside

--- like.php
+++ like.php
@@ -27,6 +27,10 @@
             if(isset($_GET["post_id"])){
                 $post_id = $_GET["post_id"];
             }
+            $total_likes = 0;
+            $user_liked="";
+
+
             /*Likes for a particular post*/
             $get_likes_query = "SELECT likes, added_by FROM posts WHERE id = ?";
             if($stmt = mysqli_prepare($con,$get_likes_query)){
@@ -36,39 +40,116 @@
                 mysqli_stmt_fetch($stmt);
                 mysqli_stmt_close($stmt);
             }
+
+
             /*Corresponding user data to liked data (needs a join so badly)*/
-            $user_details_query = "SELECT * FROM users WHERE username = ?";
-            /*
-            prepared statement
-             */
-            
-            //Like Button 
+            $user_details_query = "SELECT num_likes FROM users WHERE username = ?";
+            $total_user_likes = 0;
+            if($stmt = mysqli_prepare($con,$user_details_query)){
+                mysqli_stmt_bind_param($stmt, "s",$user_liked);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt,$total_user_likes);
+                mysqli_stmt_fetch($stmt);
+                mysqli_stmt_close($stmt);
+            }
+
+            echo "top <br>";
+            //Code for LIKE  button pressed       
+            if(isset($_POST['like_button'])){
+                echo"testing like <br>";
+                $total_likes++;
+                $make_like_query = "UPDATE posts SET likes = ? WHERE id = ?";
+                if($stmt = mysqli_prepare($con,$make_like_query)){
+                    mysqli_stmt_bind_param($stmt, "ss",$total_likes,$post_id);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                    echo "test 1/3<br>";
+                 }
+                echo "before total user like: ".$total_user_likes. "<br>";
+                $total_user_likes++;
+                echo "after total user like: ".$total_user_likes. "<br>";
+
+                $user_likes_query = "UPDATE users SET num_likes = ? WHERE username = ?";
+                if($stmt = mysqli_prepare($con,$user_likes_query)){
+                    mysqli_stmt_bind_param($stmt, "ss",$total_user_likes,$user_liked);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                    echo "test 2/3<br>";
+
+                }
+
+                $insert_user_into_likes = "INSERT INTO likes VALUES('',?,?)";
+                if($stmt = mysqli_prepare($con,$insert_user_into_likes)){
+                    mysqli_stmt_bind_param($stmt, "ss",$userLoggedIn,$post_id );
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                    echo "test 3/3<br>";
+                }
+            }
+
+                /* Insert some king of notification!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
+                /*Unlike button*/
+                if(isset($_POST['unlike_button'])){
+                echo"testing unlike <br>";
+                $total_likes--;
+                $make_like_query = "UPDATE posts SET likes = ? WHERE id = ?";
+                if($stmt = mysqli_prepare($con,$make_like_query)){
+                    mysqli_stmt_bind_param($stmt, "ss",$total_likes,$post_id);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                 }
+                $total_user_likes--;
+                $user_likes_query = "UPDATE users SET num_likes = ? WHERE username = ?";
+                if($stmt = mysqli_prepare($con,$user_likes_query)){
+                    mysqli_stmt_bind_param($stmt, "ss",$total_user_likes,$user_liked);
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                }
+                $insert_user_into_likes = "DELETE FROM likes WHERE username=? AND post_id=?";
+                if($stmt = mysqli_prepare($con,$insert_user_into_likes)){
+                    mysqli_stmt_bind_param($stmt, "ss",$userLoggedIn,$post_id );
+                    mysqli_stmt_execute($stmt);
+                    mysqli_stmt_close($stmt);
+                }
+            }
 
 
+            //Check for previous likes
+            $check_for_prevLikes_query = "SELECT COUNT(id) FROM likes WHERE username=? AND post_id = ?";
+            $num_rows = 0;
+            if($stmt = mysqli_prepare($con,$check_for_prevLikes_query)){
+                mysqli_stmt_bind_param($stmt, "ss",$userLoggedIn,$post_id);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_bind_result($stmt,$num_rows);
+                mysqli_stmt_fetch($stmt);
+                mysqli_stmt_close($stmt);
+            }
 
-            //Unlike Button
+
             
             //Check for previous likes
             $check_for_previous_likes_query = "SELECT * FROM likes WHERE username = ? AND post_id = ?";
             $num_row=0;
             /*Get number of rows of this query!!!*/
 
-            if($num_row>0){
-                echo '<form action="like.php?post_id=' .  $post_id . '" method="POST ">
-                        <input type="submit" class="comment_like" name="unlike_button" value="Unlike ">
-                        <div class="like_value"> 
-                            '.$total_likes.' Likes
-                        </div> 
-                      </form> 
-                     ';
-            }else{
-                echo '<form action="like.php?post_id=' .  $post_id . '" method="POST ">
-                        <input type="submit" class="comment_like" name="like_button" value="L ike ">
-                        <div class="like_value"> 
-                            '.$total_likes.' Likes
-                        </div> 
-                      </form> 
-                     ';
+            if($num_rows > 0) {
+                echo '<form action="like.php?post_id=' . $post_id . '" method="POST">
+                        <input type="submit" class="comment_like" name="unlike_button" value="Unlike">
+                        <div class="like_value">
+                            '. $total_likes .' Likes
+                        </div>
+                    </form>
+                ';
+            }
+            else {
+                echo '<form action="like.php?post_id=' . $post_id . '" method="POST">
+                        <input type="submit" class="comment_like" name="like_button" value="Like">
+                        <div class="like_value">
+                            '. $total_likes .' Likes
+                        </div>
+                    </form>
+                ';
             }
 
 
