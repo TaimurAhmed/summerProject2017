04161762253a69ec83338b99d32a1daab03e0b1d
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-21 20:05:27 +0300

    Basic user profile stats added

--- profile.php
+++ profile.php
@@ -3,15 +3,18 @@ require './includes/header.php';
 
 if(isset($_GET['profile_username'])){
     $username = $_GET['profile_username'];
-    $build_user_profile_query = "SELECT profile_pic FROM users WHERE username = ?";
+    $build_user_profile_query = "SELECT profile_pic,friend_array,num_posts,num_likes FROM users WHERE username = ?";
     $user_profile_array = array();
         if($stmt = mysqli_prepare($con,$build_user_profile_query)){
             mysqli_stmt_bind_param($stmt, "s",$username);
             mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt,$user_profile_array['profile_pic']);
+            mysqli_stmt_bind_result($stmt,$user_profile_array['profile_pic'],$user_profile_array['friend_array'],$user_profile_array['num_posts'],$user_profile_array['num_likes']);
             mysqli_stmt_fetch($stmt);
             mysqli_stmt_close($stmt);
         }
+    /*Count number of friends*/
+    $extraComma = 1;
+    $num_friends = substr_count($user_profile_array['friend_array'], ",") -  $extraComma ; 
 }
 
 
@@ -21,8 +24,10 @@ if(isset($_GET['profile_username'])){
         <img src="<?php echo $user_profile_array['profile_pic'];?>"/>
     </div>
 
-    <div class="profile_info">
-        
+    <div class="profile_info ">
+        <p><?php echo "Posts: ". $user_profile_array['num_posts'] ?></p>
+        <p><?php echo "Likes: ". $user_profile_array['num_likes'] ?></p>
+        <p><?php echo "Friends : ". $num_friends ?></p>
     </div>
 
     <div class="main_column column">
