0d7b681bfec189caa6ec1179597dbe8ad795edb3
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-22 12:36:37 +0300

    Add remove friend client side logic

--- profile.php
+++ profile.php
@@ -1,5 +1,7 @@
  <?php 
 require './includes/header.php';
+include("./includes/classes/User.php");
+include("./includes/classes/Post.php"); 
 
 if(isset($_GET['profile_username'])){
     $username = $_GET['profile_username'];
@@ -33,6 +35,27 @@ if(isset($_GET['profile_username'])){
             <p><?php echo "Likes: ". $user_profile_array['num_likes'] ?></p>
             <p><?php echo "Friends : ". $num_friends ?></p>
             </div>
+        
+        <form action="<?php echo $username; ?>">
+            <?php
+                $profile_user_obj  = new User($con,$username);
+                if($profile_user_obj->isClosed()){
+                    header("Location: user_closed.php");
+                }
+            
+
+            $logged_in_user_obj = new User($con,$userLoggedIn);
+            if($userLoggedIn != $username){
+                if($logged_in_user_obj->isFriend($username)){
+                    echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+                }
+            }
+            ?>
+        </form>
+
+
+
+
     </div>
 
 
---
+++ user_closed.php
@@ -0,0 +1,11 @@
+<?php
+require './includes/header.php';
+?>
+
+<div class="main_column column" id="main_column">
+    <h4>User Closed</h4>
+
+    This account is closed
+    <br>
+    <a href="index.php">Click me to get back home</a>
+</div>
