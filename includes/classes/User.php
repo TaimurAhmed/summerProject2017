a1f77d2a74415f575fa34b60a45bfa2554fd12f7
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-22 15:46:10 +0300

    Add db queries for deleting a friend

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -115,10 +115,40 @@ class User{
             mysqli_stmt_bind_result($stmt,$number);
             mysqli_stmt_fetch($stmt);
             mysqli_stmt_close($stmt);
-       }
+        }
 
        return $number > 0 ;
     }
+
+    public function removeFriend($user_to_remove){
+        $logged_in_user = $this->user['username'];
+        $get_friends_query = "SELECT friend_array FROM users WHERE username = ?";
+        if($stmt = mysqli_prepare($this->con,$get_friends_query)){
+            mysqli_stmt_bind_param($stmt, "s",$user_to_remove);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt,$friend_array);
+            mysqli_stmt_fetch($stmt);
+            mysqli_stmt_close($stmt);
+        }
+
+        /*To delete friend from users profile*/
+        $friend_array = str_replace($user_to_remove . ",", "", $friend_array);
+        deleteFriendFromArray($friend_array,$logged_in_user);
+        /*To delete friend from other persons profile too*/
+        $friend_array = str_replace($this->['username'] . ",","",$friend_array);
+        deleteFriendFromArray($friend_array,$user_to_remove);
+
+    }
+
+    private function deleteFriendFromArray($friend_array,$user_to_remove){
+        $update_friends_array = "UPDATE users SET friend_array = ?  WHERE username = ?";
+        if($stmt = mysqli_prepare($this->con,$update_friends_array)){
+            mysqli_stmt_bind_param($stmt, "ss",$friend_array,$user_to_remove);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    }
+
 }
 
 
