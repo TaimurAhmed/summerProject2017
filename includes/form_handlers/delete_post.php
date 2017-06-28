64b8abea74990f418a6a0cdcbe89d7f62fab016a
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-28 17:39:00 +0300

    Use a prepared statement instead now

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -1,14 +1,19 @@
-<?php 
-require '../../config/config.php';
-    
+<?php
+    require '../../config/config.php';
+
     if(isset($_GET['post_id']))
         $post_id = $_GET['post_id'];
-    //$post_id = "118";
-    //$query = mysqli_query($con, "UPDATE posts SET deleted='yes' WHERE id='$post_id'");
+    echo "<script>console.log('hello i am here')</script>";
+
 
     if(isset($_POST['result'])) {
-        if($_POST['result'] == 'true')
-            $query = mysqli_query($con, "UPDATE posts SET deleted='yes' WHERE id='$post_id'");
-    }
+        if($_POST['result'] == 'true'){
+            $delete_post_query = "UPDATE posts SET deleted = 'yes' WHERE id = ?";
+            if($stmt = mysqli_prepare($con,$delete_post_query)){
+                mysqli_stmt_bind_param($stmt, "s",$post_id);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
+            }
+        }
 
 ?>
