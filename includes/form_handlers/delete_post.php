fbfcfe50e53b05f72238b099aaaae0e180e1f5de
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-28 13:03:19 +0300

    Add query for deleting posts

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -4,6 +4,13 @@
     if(isset($_GET['post_id'])) 
         $post_id = $_GET['post_id'];
     if(isset($_POST['result'])){
+         $delete_post_query = "UPDATE posts SET deleted = 'yes' WHERE id = ?";
+        if($stmt = mysqli_prepare($con,$delete_post_query)){
+            mysqli_stmt_bind_param($stmt, "s",$post_id);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+
         
     }
 
