4406fc12e016c78c9b8c05a2fb0ecc65b747df6f
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-30 17:26:31 +0300

    Add html from index.php for main column

--- messages.php
+++ messages.php
@@ -19,3 +19,26 @@ if($user_to != "new"){
 }
 
 ?>
+
+    <div class ="user_details column">
+        <a href="<?php echo $userLoggedIn; ?>"><img src="
+            <?php if(isset($meta_person["profile_pic"])){echo $meta_person["profile_pic"];}?>" 
+           alt="">
+        </a>
+
+        <div class="user_details_left_right">
+            <a href="<?php echo $userLoggedIn; ?>">
+                <?php if(isset($meta_person["first_name"])){echo $meta_person["first_name"];}            
+                      if(isset($meta_person["last_name"])){echo " " . $meta_person["last_name"];}
+                ?>
+                
+            </a>
+            <br>
+            <?php if(isset($meta_person["num_posts"])){echo "Posts:". $meta_person["num_posts"]."<br>";}?>
+            <?php if(isset($meta_person["num_likes"])){echo "Likes:". $meta_person["num_likes"];}?>  
+        </div>
+    </div>
+
+
+
+    
