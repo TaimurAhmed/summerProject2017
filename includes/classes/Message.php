772d6c5f3f3270a75a0b7b5e2cc33e6d70ddc5ee
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-06-30 14:15:38 +0300

    Just a checkpost, reminder: * Check SQL for aggregation * Add a helper method for row count?

--- includes/classes/Message.php
+++ includes/classes/Message.php
@@ -1,11 +1,24 @@
   <?php
-class Message{
+class Message {
     private $user_obj;
     private $con;
 
     public function __construct($con,$user){
         $this->con = $con;
         $this->user_obj = new User($con,$user);
+    }
+
+    public function getMostRecentUser(){
+        $userLoggedIn = $this->user_obj->getUsername;
+        $getmst_recent_user_query = "SELECT user_to,user_from FROM messages WHERE user_to = ? OR user_from = ? ORDER BY id DESC LIMIT 1";
+        if($stmt = mysqli_prepare($con,$getmst_recent_user_query)){
+            mysqli_stmt_bind_param($stmt, "ss",$userLoggedIn,$userLoggedIn);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt,$result);
+            mysqli_stmt_fetch($stmt);
+            mysqli_stmt_close($stmt);
+        }
+        
     }
  
  }
---
+++ messages.php
@@ -0,0 +1,18 @@
+<?php
+include("./includes/header.php");
+
+$message_obj = new Message($con,$userLoggedIn);
+
+//Username is already taken as value in the assoc array
+if(isset($_GET['u'])){
+    $user_to = $_GET['u'];
+}else{
+    //Assuming user has not messaged anyone yet
+    $user_to = $message_obj->getMostRecentUser();
+    if($user_to === false){
+        $user_to = 'new';
+    }
+}
+
+
+?>
