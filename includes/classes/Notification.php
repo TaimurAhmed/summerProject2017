60943c0f5a8123f028e5467d80ba58b0c2bd0074
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-07-10 13:43:09 +0100

    New methods to notifications (not tested yet)

--- includes/classes/Notification.php
+++ includes/classes/Notification.php
@@ -52,13 +52,151 @@ class Notification {
         $insert_query = mysqli_query($this->con, "INSERT INTO notifications VALUES('', '$user_to', '$userLoggedIn', '$message', '$link', '$date_time', 'no', 'no')");
     }
 
+    private function checkForNotifications($userLoggedIn){
+        $count_notifications = "SELECT COUNT(id) FROM notifications WHERE user_to= ?";
+        $result = 0;
+        if($stmt = mysqli_prepare($this->con,$count_notifications)){
+            mysqli_stmt_bind_param($stmt, "s",$userLoggedIn);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt,$result);
+            mysqli_stmt_fetch($stmt);
+            mysqli_stmt_close($stmt);
+        }
+
+        return $result > 0;
+    }
+
+    private function getTimeStamp($date_time){
+        $date_time_now = date("Y-m-d H:i:s");
+        $start_date = new DateTime($date_time); //Time of post
+        $end_date = new DateTime($date_time_now); //Current time
+        $interval = $start_date->diff($end_date); //Difference between dates 
+        if($interval->y >= 1) {
+            if($interval == 1)
+                $time_message = $interval->y . " year ago"; //1 year ago
+            else 
+                $time_message = $interval->y . " years ago"; //1+ year ago
+        }
+        else if ($interval->m >= 1) {
+            if($interval->d == 0) {
+                $days = " ago";
+            }
+            else if($interval->d == 1) {
+                $days = $interval->d . " day ago";
+            }
+            else {
+                $days = $interval->d . " days ago";
+            }
+
+
+            if($interval->m == 1) {
+                $time_message = $interval->m . " month". $days;
+            }
+            else {
+                $time_message = $interval->m . " months". $days;
+            }
+
+        }
+        else if($interval->d >= 1) {
+            if($interval->d == 1) {
+                $time_message = "Yesterday";
+            }
+            else {
+                $time_message = $interval->d . " days ago";
+            }
+        }
+        else if($interval->h >= 1) {
+            if($interval->h == 1) {
+                $time_message = $interval->h . " hour ago";
+            }
+            else {
+                $time_message = $interval->h . " hours ago";
+            }
+        }
+        else if($interval->i >= 1) {
+            if($interval->i == 1) {
+                $time_message = $interval->i . " minute ago";
+            }
+            else {
+                $time_message = $interval->i . " minutes ago";
+            }
+        }
+        else {
+            if($interval->s < 30) {
+                $time_message = "Just now";
+            }
+            else {
+                $time_message = $interval->s . " seconds ago";
+            }
+        }
+
+        return $time_message
+    }
+
+    public function getNotifications($data,$limit){
+
+        $page = $data['page'];
+        $userLoggedIn = $this->user_obj->getUsername();
+        $return_string = "";
+
+        if($page == 1)
+            $start = 0;
+        else 
+            $start = ($page - 1) * $limit;
+
+        $set_viewed_query = "UPDATE notifications SET viewed='yes' WHERE user_to=?";
+        if($stmt = mysqli_prepare($this->con,$set_viewed_query)){
+            mysqli_stmt_bind_param($stmt, "s",$userLoggedIn);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
 
 
 
+        if(checkForNotifications($userLoggedIn)) {
+            echo "You have no notifications!";
+            return;
+        }
 
+        $num_iterations = 0; //Number of messages checked 
+        $count = 1; //Number of messages posted
+        $get_all_notifcation = "SELECT user_from,datetime,profile_pic,opened,link,message FROM notifications JOIN users ON user_from=username WHERE user_to=? ORDER BY notifications.id DESC";
+        if($stmt = mysqli_prepare($this->con,$set_viewed_query)){
+            mysqli_stmt_bind_param($stmt, "s",$userLoggedIn);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt,$user_from,$datetime,$profile_pic,$opened,$link,$message);
+            while(mysqli_stmt_fetch($stmt)){
+                if($num_iterations++ < $start)
+                    continue;
+                if($count > $limit)
+                    break;
+                else 
+                    $count++;
+                $this->getTimeStamp($datetime);
 
+                $style = ($opened == 'no') ? "background-color: #DDEDFF;" : "";
 
+                $return_string .= "<a href='" . $link . "'> 
+                                        <div class='resultDisplay resultDisplayNotification' style='" . $style . "'>
+                                            <div class='notificationsProfilePic'>
+                                                <img src='" . $profile_pic . "'>
+                                            </div>
+                                            <p class='timestamp_smaller' id='grey'>" . $time_message . "</p>" . $message . "
+                                        </div>
+                                    </a>";
 
+            }
+            mysqli_stmt_close($stmt);
+        }
+
+        //If posts were loaded
+        if($count > $limit)
+            $return_string .= "<input type='hidden' class='nextPageDropdownData' value='" . ($page + 1) . "'><input type='hidden' class='noMoreDropdownData' value='false'>";
+        else 
+            $return_string .= "<input type='hidden' class='noMoreDropdownData' value='true'> <p style='text-align: center;'>No more notifications to load!</p>";
+
+        return $return_string;
+    }
 }
 ?>
 
--- includes/handlers/ajax_load_notification.php
+++ includes/handlers/ajax_load_notification.php
@@ -1,1 +1,14 @@
- 
+ <?php
+include("../../config/config.php");
+include("../classes/User.php");
+include("../classes/Notification.php");
+
+
+/*Number of messages to Load*/
+$limit = 7;
+
+$notification = new Notification($con, $_REQUEST['userLoggedIn']);//Request from Ajax call
+
+echo $notification->getNotification($_REQUEST, $limit);
+
+?>
