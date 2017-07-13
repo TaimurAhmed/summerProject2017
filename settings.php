0783187a6e7f591294abbe14c182c18e30d3e966
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-07-13 19:39:47 +0100

    Add settings.php

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php 
+include("includes/header.php");
+//include("includes/settings_handler.php");
+
+?>
+
+<div class="main_column column ">
+
+    <h4> Account Settings</h4>
+    
+    <?php
+        echo "<img src='" .$userMeta['profile_pic']. "'/>";
+    ?>
+    <br>
+    <a href="upload.php">Upload New Profile Picture</a><br><br><br>
+</div>
