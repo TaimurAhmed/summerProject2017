c9d8f67a126fbd922bb1aa100a6d5c89dd47bba6
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-07-20 14:33:59 +0100

    config.php house keeping and finishing touches

--- config/config.php
+++ config/config.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 See:
 
@@ -8,24 +7,19 @@ Reference of implementation: https://www.youtube.com/watch?v=KnX0p2Ey3Ek
 
 */
 
-/*Turns on output buffering*/
-ob_start(); 
-
-/*Set: Cookie expiration/lifetime, cookie works on all domain paths, cookie domains, no SSL, http only i.e. to prevent JS injection*/
-/*Reccomended: Consider using an SSL protocol to prevent session hijacking. In that case set parameter to true instead*/
-session_set_cookie_params(time()+30,'/','localhost',false,true);
-session_start();
-
-/*Set a random session variable. My alternative to binding to IP, as this would cause problems on open Wifi*/
-$_SESSION["ses_var"] = "green";
-
-$time_zone = date_default_timezone_set("Europe/London");
-
-$con = mysqli_connect("localhost","root","","social"); /*Connection Variable*/
-
-if(mysqli_connect_errno()){
-   echo "Connection failed: ". mysqli_connect_errno();
-   exit();
-}
+    /*Turns on output buffering*/
+    ob_start(); 
+    /*Set: Cookie expiration/lifetime, cookie works on all domain paths, cookie domains, no SSL, http only i.e. to prevent JS injection*/
+    /*Reccomended: Consider using an SSL protocol to prevent session hijacking. In that case set parameter to true instead*/
+    session_set_cookie_params(time()+30,'/','localhost',false,true);
+    session_start();
+    /*Set a random session variable. My alternative to binding to IP, as this would cause problems on open Wifi*/
+    $_SESSION["ses_var"] = "green";
+    $time_zone = date_default_timezone_set("Europe/London");
+    $con = mysqli_connect("localhost","root","","social"); /*Connection Variable*/
+    if(mysqli_connect_errno()){
+       echo "Connection failed: ". mysqli_connect_errno();
+       exit();
+    }
 
 ?>
