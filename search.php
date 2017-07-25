4e89ba5e45a93a30ad176fda1b366a08567949d9
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-07-25 21:23:50 +0100

    User Feedback: Refactored search page so that for loop will not be entered if no results found. i.e. program was trying to iterate through a list of results that do not exist

--- search.php
+++ search.php
@@ -132,81 +132,84 @@ if(isset($_GET['type'])){
                     }
                 }
 
+
+                echo "<p id='grey'>Try searching for:</p>";
+                        echo "<a href='search.php?q=" . $query ."&type=name'>Names</a>, <a href='search.php?q=" . $query ."&type=username'>Usernames</a><br><br><hr id='search_hr'>";
+
                 if(!$n){
                     echo "Couldnt find anyone with ". $type. " like: ". $query;
                 }else{
-                    echo $n . " results found:".$queryCount." <br> <br>";
-                }
-                echo "<p id='grey'>Try searching for:</p>";
-                echo "<a href='search.php?q=" . $query ."&type=name'>Names</a>, <a href='search.php?q=" . $query ."&type=username'>Usernames</a><br><br><hr id='search_hr'>";
+                        echo $n . " results found:".$queryCount." <br> <br>";
+                        
 
 
-                for($i=0;$i<=$queryCount;$i++){
-                    $user_obj = new User($con, $userMeta['username']);//From header file
-                    $button = "";
-                    $mutual_friends = "";
-                    //echo "HERE" . $userMeta['username'] . $username[$i];
-                    if($userMeta['username'] != $username[$i]) {//If not the user themselves
-                        //Generate button depending on friendship status and with individual names...(class from bootstrap)
-                        if($user_obj->isFriend($username[$i])){
-                            $button = "<input type='submit' name='" . $username[$i] . "' class='danger' value='Remove Friend'>";
-                        }else if($user_obj->didRecieveRequest($username[$i])){
-                            $button = "<input type='submit' name='" . $username[$i] . "' class='warning' value='Respond to request'>";
-                        }else if($user_obj->didSendRequest($username[$i])){
-                            $button = "<input type='submit' class='default' value='Request Sent'>";
-                        }else{ 
-                            $button = "<input type='submit' name='" . $username[$i] . "' class='success' value='Add Friend'>";
-                        }
-                    }
-                    $mutual_friends = $user_obj->getMutualFriends($username[$i]) . " friends in common";
+                        for($i=0;$i<=$queryCount;$i++){
+                            $user_obj = new User($con, $userMeta['username']);//From header file
+                            $button = "";
+                            $mutual_friends = "";
+                            //echo "HERE" . $userMeta['username'] . $username[$i];
+                            if($userMeta['username'] != $username[$i]) {//If not the user themselves
+                                //Generate button depending on friendship status and with individual names...(class from bootstrap)
+                                if($user_obj->isFriend($username[$i])){
+                                    $button = "<input type='submit' name='" . $username[$i] . "' class='danger' value='Remove Friend'>";
+                                }else if($user_obj->didRecieveRequest($username[$i])){
+                                    $button = "<input type='submit' name='" . $username[$i] . "' class='warning' value='Respond to request'>";
+                                }else if($user_obj->didSendRequest($username[$i])){
+                                    $button = "<input type='submit' class='default' value='Request Sent'>";
+                                }else{ 
+                                    $button = "<input type='submit' name='" . $username[$i] . "' class='success' value='Add Friend'>";
+                                }
+                            }
+                            $mutual_friends = $user_obj->getMutualFriends($username[$i]) . " friends in common";
 
 
-                    //Button forms
-                    if(isset($_POST[$username[$i]])) {
+                            //Button forms
+                            if(isset($_POST[$username[$i]])) {
 
-                        if($user_obj->isFriend($username[$i])) {
-                            $user_obj->removeFriend($username[$i]);
-                            header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+                                if($user_obj->isFriend($username[$i])) {
+                                    $user_obj->removeFriend($username[$i]);
+                                    header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 
-                        }
-                        else if($user_obj->didRecieveRequest($username[$i])) {
-                            header("Location: requests.php");
-                        }
-                        else if($user_obj->didSendRequest($username[$i])) {
+                                }
+                                else if($user_obj->didRecieveRequest($username[$i])) {
+                                    header("Location: requests.php");
+                                }
+                                else if($user_obj->didSendRequest($username[$i])) {
 
-                        }
-                        else {
-                            $user_obj->sendRequest($username[$i]);
-                            header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-                        }
+                                }
+                                else {
+                                    $user_obj->sendRequest($username[$i]);
+                                    header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+                                }
 
-                    }
+                            }
 
 
 
-                echo "<div class='search_result_indiv'>
-                            <div class='searchPageFriendButtons'>
-                                <form action='' method='POST'>
-                                    " . $button . "
+                        echo "<div class='search_result_indiv'>
+                                    <div class='searchPageFriendButtons'>
+                                        <form action='' method='POST'>
+                                            " . $button . "
+                                            <br>
+                                        </form>
+                                    </div>
+
+
+                                    <div class='result_profile_pic'>
+                                        <a href='" . $username[$i] ."'><img src='". $profilePic[$i] ."' style='height: 100px;'></a>
+                                    </div>
+
+                                    <a href='" . $username[$i] ."'> " . $firstName[$i] . " " . $lastName[$i] . "
+                                    <p id='grey'> " . $username[$i] ."</p>
+                                    </a>
                                     <br>
-                                </form>
-                            </div>
+                                    " . $mutual_friends ."
+                                    <br>
 
+                                </div>
+                                <hr id='search_hr'>";
 
-                            <div class='result_profile_pic'>
-                                <a href='" . $username[$i] ."'><img src='". $profilePic[$i] ."' style='height: 100px;'></a>
-                            </div>
-
-                            <a href='" . $username[$i] ."'> " . $firstName[$i] . " " . $lastName[$i] . "
-                            <p id='grey'> " . $username[$i] ."</p>
-                            </a>
-                            <br>
-                            " . $mutual_friends ."
-                            <br>
-
-                        </div>
-                        <hr id='search_hr'>";
-
+                        }
                 }
 
 
