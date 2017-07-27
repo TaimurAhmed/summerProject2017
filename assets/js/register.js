3dbdc934400ed0bf315c8fca355509f99b6a875e
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-07-27 18:49:38 +0100

    WAI-ARIA: Change aria-hidden attribute on dynamic registration and sign in form

--- assets/js/register.js
+++ assets/js/register.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
     
     //On click , hide login and show registration form
     $("#signup").click(function() {
+        
         /* Act on the event */
         $("#first_form").slideUp("slow",function(){
             $("#second_form").slideDown("slow");
         });
+
     });
+
+
 
     //On click , hide login and show registration form
     $("#signin").click(function() {
@@ -15,6 +19,18 @@ $(document).ready(function() {
         $("#second_form").slideUp("slow",function(){
             $("#first_form").slideDown("slow");
         });
+
+
+    });
+
+    $("#signup").click(function(){
+        $("#first_form").attr("aria-hidden", "true");
+        $("#second_form").attr("aria-hidden", "false");
+    });
+
+    $("#signin").click(function(){
+        $("#second_form").attr("aria-hidden", "true");
+        $("#first_form").attr("aria-hidden", "false");
     });
 
 });
