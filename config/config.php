c9501c1e798a425065cb4804261fae71bcf8e107
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-08-15 14:44:47 +0100

    SHowing ollie something delete this later

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
+
+/*Show ollie something!!!!!!!!!!!!!!!!!!*/
 /*
 See:
 
@@ -24,3 +26,4 @@ Reference of implementation: https://www.youtube.com/watch?v=KnX0p2Ey3Ek
     $con->set_charset('utf8'); //Check API !!!!! Many new attack vectors rely on encoding bypassing. Use UTF-8 as your database and application charset unless you have a mandatory requirement to use another encoding
 
 ?>
+
