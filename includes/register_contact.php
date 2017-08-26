c77374c50f44938c789d951cb438ce38db2b7e21
Taimur Ahmed <ta16969@my.bristol.ac.uk>
2017-08-26 16:49:31 +0100

    Correct as per WAI-ARIA standard

--- includes/register_contact.php
+++ includes/register_contact.php
@@ -1,18 +1,20 @@
 
 
 <!--About the Artemis Project-->
-<a aria-label="View information about the Artemis Project" title="About the Artemis Project" href="https://github.com/TaimurAhmed/summerProject2017/wiki" class="sign_up_about">
+<div id='Information' aria-labelledby="Information">
+<a id='artemisInformation' artemis-labelledby='Information artemisInformation'  title="About the Artemis Project" href="https://github.com/TaimurAhmed/summerProject2017/wiki" class="sign_up_about">
     <i class="fa fa-question-circle-o fa-2x register_page_icons" aria-hidden="true"></i>  
 </a>
 &nbsp;
 <!--About the Team behind the Project-->
-<a aria-label="View information about creators of Artemis" title="The People Behind Artemis" href="https://github.com/TaimurAhmed" class="sign_up_about">
+<a id='artemisTeam' artemis-labelledby='Information artemisTeam' title="The People Behind Artemis" href="https://github.com/TaimurAhmed" class="sign_up_about">
     <i class="fa fa-github fa-2x register_page_icons" aria-hidden="true"></i>
 </a>
 &nbsp;
 <!--Report a problem-->
-<a aria-label="Report a bug or issue" title="Report a Bug or Issue" href="https://github.com/TaimurAhmed/summerProject2017/issues" class="sign_up_about">
+<a id='Help' artemis-labelledby='Information Help' title="Report a Bug or Issue" href="https://github.com/TaimurAhmed/summerProject2017/issues" class="sign_up_about">
     <i class="fa fa-bug fa-2x register_page_icons" aria-hidden="true"></i>
 </a>
+</div>
 
 
